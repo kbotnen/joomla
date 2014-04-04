4d5d0c55e070119439634a3ca3ccbd418f777326
Kristian Botnen <kristian.botnen@adm.uib.no>
2014-04-05 00:28:05 +0200

    added controller and first view

---
+++ com_mustore/site/controller.php
@@ -0,0 +1,13 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla controller library
+jimport('joomla.application.component.controller');
+ 
+/**
+ * MuStore Component Controller
+ */
+class MuStoreController extends JControllerLegacy
+{
+}
---
+++ com_mustore/site/views/mustore/view.html.php
@@ -0,0 +1,22 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla view library
+jimport('joomla.application.component.view');
+ 
+/**
+ * HTML View class for the MuStore Component
+ */
+class MuStoreViewMuStore extends JViewLegacy
+{
+        // Overwriting JView display method
+        function display($tpl = null) 
+        {
+                // Assign data to the view
+                $this->msg = 'Mu Store';
+ 
+                // Display the view
+                parent::display($tpl);
+        }
+}
