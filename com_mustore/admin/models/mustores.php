622f185e07b9c06e53037f8043b69589516aa25a
Kristian Botnen <kristian.botnen@adm.uib.no>
2014-04-05 12:11:31 +0200

    starting mvc for backend

---
+++ com_mustore/admin/models/mustores.php
@@ -0,0 +1,25 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+// import the Joomla modellist library
+jimport('joomla.application.component.modellist');
+/**
+ * MuStoreList Model
+ */
+class MuStoreModelMuStores extends JModelList {
+  /**
+   * Method to build an SQL query to load the list data.
+   *
+   * @return      string  An SQL query
+   */
+  protected function getListQuery() {
+    // Create a new query object.
+    $db = JFactory::getDBO();
+    $query = $db -> getQuery(true);
+    // Select some fields from the hello table
+    $query -> select('id,heading') -> from('#__mustores');
+
+    return $query;
+  }
+
+}
---
+++ com_mustore/admin/views/mustores/tmpl/default_head.php
@@ -0,0 +1,9 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+?>
+<tr>
+  <th width="20"><?php echo JHtml::_('grid.checkall'); ?></th>
+  <th><?php echo JText::_('MuStore Admin Greeting'); ?></th>
+  <th width="5"><?php echo JText::_('MuStore Admin Id'); ?></th>
+</tr>
