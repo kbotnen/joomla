a763e3597b8066a39a4862920f59537caa9cb9a3
Kristian Botnen <kristian.botnen@adm.uib.no>
2014-04-05 08:37:16 +0200

    added first model

--- com_mustore/site/controller.php
+++ com_mustore/site/controller.php
@@ -1,13 +1,12 @@
 <?php
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
+
 // import Joomla controller library
 jimport('joomla.application.component.controller');
- 
+
 /**
  * MuStore Component Controller
  */
-class MuStoreController extends JControllerLegacy
-{
+class MuStoreController extends JControllerLegacy {
 }
--- com_mustore/site/views/mustore/view.html.php
+++ com_mustore/site/views/mustore/view.html.php
@@ -1,22 +1,26 @@
 <?php
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
+
 // import Joomla view library
 jimport('joomla.application.component.view');
- 
+
 /**
  * HTML View class for the MuStore Component
  */
-class MuStoreViewMuStore extends JViewLegacy
-{
-        // Overwriting JView display method
-        function display($tpl = null) 
-        {
-                // Assign data to the view
-                $this->msg = 'Mu Store';
- 
-                // Display the view
-                parent::display($tpl);
-        }
+class MuStoreViewMuStore extends JViewLegacy {
+  // Overwriting JView display method
+  function display($tpl = null) {
+    // Assign data to the view
+    $this -> msg = $this -> get('Msg');
+
+    // Check for errors.
+    if (count($errors = $this -> get('Errors'))) {
+      JLog::add(implode('<br />', $errors), JLog::WARNING, 'jerror');
+      return false;
+    }
+    // Display the view
+    parent::display($tpl);
+  }
+
 }
