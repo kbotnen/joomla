1fb8560ddc08a19308ff2545134b688f48d63375
Kristian Botnen <kristian.botnen@adm.uib.no>
2014-04-06 10:12:19 +0200

    added more fields to the database

--- com_mustore/admin/sql/install.mysql.utf8.sql
+++ com_mustore/admin/sql/install.mysql.utf8.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS `#__mustore`;
  
 CREATE TABLE `#__mustore` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `heading` varchar(25) NOT NULL,
+  `heading` varchar(255) NOT NULL,
+  `type` varchar(25) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `filepath` varchar(255) NOT NULL,  
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
  
-INSERT INTO `#__mustore` (`heading`) VALUES
-        ('This is your MuStore'),
-        ('Welcome to MuStore');
